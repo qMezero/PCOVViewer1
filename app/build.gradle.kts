50798be7b27316878fa167cdef0ea27fad3a4225
qMezero <138614258+qMezero@users.noreply.github.com>
2025-11-02 00:18:45 +0300

    Add Kotlin test dependency for unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     // PDF-библиотека iText
     implementation("com.itextpdf:itextpdf:5.5.13.3")
 
+    testImplementation(kotlin("test"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
